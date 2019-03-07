52a55be6d15b1d4283bbfd9abcfb75c082c04cd5
unknown <xdq@home.xdq.me>
2019-03-07 20:57:56 +0800

    add comment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import fs from 'fs'
 import path from 'path'
 import util from 'util'
 
+/**
+ * write some message to file
+ * @param message message to write
+ * @param fleName main file name of file
+ * @param fileExtension file extension of the file
+ * @param timeStamp add time stamp or not
+ */
 export function dumpToFileSync(message: any, fleName: string = 'dump', fileExtension: string = 'log', timeStamp: boolean = true) {
     let fullFileName
     if (timeStamp) {
